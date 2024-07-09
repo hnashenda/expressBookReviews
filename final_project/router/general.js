895c8f94ba1483eb4a81d6f14bdb74b071da779d
Hubert Nashenda <hubert.nashenda@gmail.com>
2024-07-09 19:40:34 +0200

    Update general.js
    list of books

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -13,7 +13,9 @@ public_users.post("/register", (req,res) => {
 // Get the book list available in the shop
 public_users.get('/',function (req, res) {
   //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+  //return res.status(300).json({message: "Yet to be implemented"});
+  const title = req.params.email;
+  res.send(books[title]);
 });
 
 // Get book details based on ISBN
